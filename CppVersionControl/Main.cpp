055b44c1aae60810e9b99c88d19796ffaa39e2c3
Welinton Azevedo <welintonz226@gmail.com>
2025-07-24 20:54:00 -0300

    Added GoodBye Funciton

--- CppVersionControl/Main.cpp
+++ CppVersionControl/Main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 
-int main() 
+void Greet()
 {
 	// Print some text
 	std::cout << "Welcome!\n\n";
+}
+
+void GoogBye()
+{
 	std::cout << "GoodBye!\n\n";
+}
+
+
+int main() 
+{
+	Greet();
+	GoogBye();
 	system("pause");
 }
